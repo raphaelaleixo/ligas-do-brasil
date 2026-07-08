247ad01d2cec1b8c94f511b812dc117b5184078c
Raphael Aleixo <raphael.aleixo@gmail.com>
2026-07-08 15:35:30 -0400

    chore: configure vitest

---
+++ vitest.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['tests/**/*.test.js'],
+    globals: false,
+    environment: 'node',
+    reporters: 'default',
+  },
+});
